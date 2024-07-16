b46e31ab66f44cda0c19715f1785a403db80f55e
CaiXiaobo <424559864@qq.com>
2024-07-16 15:39:49 +0800

    commit lru_upgrade

--- interview/hand_cut/lru_cache/lru_upgrade.go
+++ interview/hand_cut/lru_cache/lru_upgrade.go
@@ -1,5 +1,10 @@
 package lru_cache
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 传统的lru会出现预读失败和缓冲池污染的问题
 
@@ -14,43 +19,169 @@ package lru_cache
 只有同时满足“被访问”，并且“在老生代停留时间大于T”，才会被放入新生代头部
 */
 
-type UpgradeLRUCache struct {
-	size                               int
-	limit                              int
-	newSize                            int
-	oldSize                            int
-	newLimit                           int
-	oldLimit                           int
-	timeWin                            int
-	cache                              map[string]*DoubleList
-	newHead, newTail, oldHead, oldTail *DoubleList
+type GenerationType int
+
+const (
+	OldGen GenerationType = iota
+	NewGen
+)
+
+// LRUCacheUpgrade 缓存
+type LRUCacheUpgrade struct {
+	size             int
+	limit            int
+	oldLimit         int
+	cache            map[string]*DoubleLinkListUpgrade
+	headOld, tailOld *DoubleLinkListUpgrade
+	headNew, tailNew *DoubleLinkListUpgrade
+	windowTime       time.Duration
 }
 
-type DoubleList struct {
-	key       string
-	value     int
-	t         int // 在老生代停留时间
-	pre, next *DoubleList
+// DoubleLinkListUpgrade 双链表
+type DoubleLinkListUpgrade struct {
+	key        string
+	value      int
+	timestamp  time.Time
+	generation GenerationType
+	next, pre  *DoubleLinkListUpgrade
 }
 
-// PreRead 预读
-func (ulc *UpgradeLRUCache) PreRead(nodes ...*DoubleList) {
-	for _, node := range nodes {
-		ulc.PutNodeToOldHead(node)
-	}
-}
-
-func (ulc *UpgradeLRUCache) Get(key string) int {
-	if _, ok := ulc.cache[key]; !ok {
+// Get 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1；
+func (lc *LRUCacheUpgrade) Get(key string) int {
+	if node, ok := lc.cache[key]; !ok {
 		return -1
+	} else {
+		if lc.isOldGen(node) {
+			// 如果在老生代，检查停留时间
+			if time.Since(node.timestamp) > lc.windowTime {
+				lc.moveNodeToNewGen(node)
+			} else {
+				lc.moveNodeToHead(node, lc.headOld)
+			}
+		} else {
+			lc.moveNodeToHead(node, lc.headNew)
+		}
+		return node.value
 	}
-	node := ulc.cache[key]
-	ulc.moveNodeToHead(node)
-	return node.value
 }
 
-func (ulc *UpgradeLRUCache) PutNodeToOldHead(node *DoubleList) {
-	if node, ok := ulc.cache[key]; !ok {
-
+// Put 如果关键字 key 存在于缓存中，则变更其数据值 value；如果不存在，则向缓存中插入该 key，value。
+// 如果插入操作导致导致 key 数量超过缓存容量，则应该逐出最久未使用的 key；
+func (lc *LRUCacheUpgrade) Put(key string, value int, isPrefetch bool) {
+	if node, ok := lc.cache[key]; !ok {
+		node = &DoubleLinkListUpgrade{
+			key:       key,
+			value:     value,
+			timestamp: time.Now(),
+		}
+		lc.cache[key] = node
+		if isPrefetch {
+			lc.addNodeToHead(node, lc.headOld)
+		} else {
+			lc.addNodeToHead(node, lc.headNew)
+		}
+		lc.size++
+		lc.evict()
+	} else {
+		node.value = value
+		if lc.isOldGen(node) {
+			// 如果在老生代，检查停留时间
+			if time.Since(node.timestamp) > lc.windowTime {
+				lc.moveNodeToNewGen(node)
+			} else {
+				lc.moveNodeToHead(node, lc.headOld)
+			}
+		} else {
+			lc.moveNodeToHead(node, lc.headNew)
+		}
 	}
+}
+
+// moveNodeToNewGen 将节点从老生代移动到新生代
+func (lc *LRUCacheUpgrade) moveNodeToNewGen(node *DoubleLinkListUpgrade) {
+	lc.removeNode(node)
+	lc.addNodeToHead(node, lc.headNew)
+	node.generation = NewGen
+}
+
+// moveNodeToHead 将节点移动到链表头部
+func (lc *LRUCacheUpgrade) moveNodeToHead(node *DoubleLinkListUpgrade, head *DoubleLinkListUpgrade) {
+	lc.removeNode(node)
+	lc.addNodeToHead(node, head)
+}
+
+// removeNode 从链表中删除节点
+func (lc *LRUCacheUpgrade) removeNode(node *DoubleLinkListUpgrade) {
+	node.pre.next = node.next
+	node.next.pre = node.pre
+}
+
+// addNodeToHead 将节点添加到指定链表头部
+func (lc *LRUCacheUpgrade) addNodeToHead(node *DoubleLinkListUpgrade, head *DoubleLinkListUpgrade) {
+	node.next = head.next
+	node.pre = head
+	head.next.pre = node
+	head.next = node
+}
+
+// evict 移除超出容量的节点
+func (lc *LRUCacheUpgrade) evict() {
+	if lc.size <= lc.limit {
+		return
+	}
+
+	// 首先移除老生代中的节点
+	for lc.size > lc.limit && lc.tailOld.pre != lc.headOld {
+		lc.removeNode(lc.tailOld.pre)
+		delete(lc.cache, lc.tailOld.pre.key)
+		lc.size--
+	}
+
+	// 然后移除新生代中的节点
+	for lc.size > lc.limit && lc.tailNew.pre != lc.headNew {
+		lc.removeNode(lc.tailNew.pre)
+		delete(lc.cache, lc.tailNew.pre.key)
+		lc.size--
+	}
+}
+
+// isOldGen 判断节点是否在老生代
+func (lc *LRUCacheUpgrade) isOldGen(node *DoubleLinkListUpgrade) bool {
+	// 如果 node 的前一个节点是老生代的头结点，则 node 属于老生代
+	return node.generation == OldGen
+}
+
+// ListLRUCache 打印缓存数据
+func (lc *LRUCacheUpgrade) ListLRUCache() {
+	fmt.Println("Old Generation:")
+	node := lc.headOld.next
+	for node != lc.tailOld {
+		fmt.Printf("key: %s, value: %d\n", node.key, node.value)
+		node = node.next
+	}
+	fmt.Println("New Generation:")
+	node = lc.headNew.next
+	for node != lc.tailNew {
+		fmt.Printf("key: %s, value: %d\n", node.key, node.value)
+		node = node.next
+	}
+}
+
+// NewLRUCacheUpgrade 初始化缓存
+func NewLRUCacheUpgrade(limit int, windowTime time.Duration) *LRUCacheUpgrade {
+	lc := &LRUCacheUpgrade{
+		limit:      limit,
+		oldLimit:   limit / 2,
+		cache:      make(map[string]*DoubleLinkListUpgrade),
+		windowTime: windowTime,
+		headOld:    &DoubleLinkListUpgrade{key: "headOld", value: 0, generation: OldGen},
+		tailOld:    &DoubleLinkListUpgrade{key: "tailOld", value: 0, generation: OldGen},
+		headNew:    &DoubleLinkListUpgrade{key: "headNew", value: 0, generation: NewGen},
+		tailNew:    &DoubleLinkListUpgrade{key: "tailNew", value: 0, generation: NewGen},
+	}
+	lc.headOld.next = lc.tailOld
+	lc.tailOld.pre = lc.headOld
+	lc.headNew.next = lc.tailNew
+	lc.tailNew.pre = lc.headNew
+	return lc
 }
