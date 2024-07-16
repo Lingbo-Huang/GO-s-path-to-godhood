03dad42454cad069813ffc6b256d7bb76767a140
CaiXiaobo <424559864@qq.com>
2024-07-16 15:53:25 +0800

    commit three_sum

--- leetcode/hot100/double_pointer/three_sum/three_sum.go
+++ leetcode/hot100/double_pointer/three_sum/three_sum.go
@@ -2,17 +2,34 @@ package three_sum
 
 import "sort"
 
-func threeSum(nums []int) [][]int {
+// leetcode 15
+func ThreeSum(nums []int) [][]int {
 	n := len(nums)
+	// 从小到大排序
 	sort.Ints(nums)
 	ans := make([][]int, 0)
 
-	for i, num := range nums {
-		if num > 0 {
-			return ans
-		}
-		if i > 0 && nums[i] == nums[i-1] {
+	for first := 0; first < n; first++ {
+		if first > 0 && nums[first] == nums[first-1] {
 			continue
 		}
+		third := n - 1
+		// 求解两数之和
+		target := -1 * nums[first]
+		for second := first + 1; second < n; second++ {
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
 	}
+	return ans
 }
